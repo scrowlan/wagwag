ace050148f8945f16d2451e5bc325214ec75abcb
Jenny <jenmjayne@gmail.com>
2014-11-17 13:33:02 -0600

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Wagwag::Application.config.secret_key_base = 'ba1803f8a233ef3a3227180dc0b493f8709e01b6ced1a9a8beaafced8da9306d84c31f7b157b58f64f770ed7ef71026bed874ac605ef3ccf8de826288d1c3370'
