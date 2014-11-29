a6c0d69663053322fe60441763f3dd2fec60aba4
Tyler Mikev <atmikev@gmail.com>
2014-11-29 14:12:12 -0500

    Create Pet scaffold.

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141128162338) do
+ActiveRecord::Schema.define(version: 20141129043444) do
 
   create_table "accounts", force: true do |t|
     t.integer  "user_id"
@@ -65,6 +65,9 @@ ActiveRecord::Schema.define(version: 20141128162338) do
     t.string   "name"
     t.string   "password_digest"
     t.string   "role"
+    t.string   "first_name"
+    t.string   "last_name"
+    t.string   "picture"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
